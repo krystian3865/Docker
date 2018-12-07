c1410e1575b9fd332447f52a029145b6ad62c089
krystian3865 <32763753+krystian3865@users.noreply.github.com>
2018-12-07 16:19:44 +0100

    Update MysqlCon.java

--- MysqlCon.java
+++ MysqlCon.java
@@ -35,7 +35,7 @@ public static void main(String args[]) {
     {
       ResultSet rs=stmt.executeQuery("select * from tabela");
       while(rs.next())
-      System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "rs.getInt(3));
+      System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3));
     } else
     {
       System.out.println("Nie ma takiej opcji!");
